7da7e055c66f506c27335b053b9cebc2f468b112
Sunny <mqdlad@gmail.com>
2025-05-28 18:28:55 +0530

    rebase commit

---
+++ frontend/src/Pages/Authentication.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Login from '../Components/Login'
+import Signup from '../Components/Signup'
+
+const Authentication = () => {
+  return (
+    <div>
+      <h1>Hello world!</h1>
+    </div>
+  )
+}
+
+export default Authentication
